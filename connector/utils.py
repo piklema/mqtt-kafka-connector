83ac866cd0a7ce689e21a4b928a50349c324629f
Denis Eliseev <d.a.eliseev@gmail.com>
2022-12-07 15:51:25 +0300

    DA-379: feat add template transformation

--- connector/utils.py
+++ connector/utils.py
@@ -1,6 +1,40 @@
+import re
+
 def prepare_topic_mqtt_to_kafka(mqtt_topic: str) -> str:
     try:
         customer_id = mqtt_topic.split('/')[1]
     except IndexError:
         return ''
     return f'customer_{customer_id}'
+
+
+class Template:
+    MASK_REGEXP = r"{(?P<tpl_name>\w+)}"
+    NOT_USED_SYM = '~'
+
+    def __init__(self, src_tpl: str, dst_tpl: str):
+        self.src_tpl = src_tpl  # Исходный шаблон
+        self.dst_tpl = dst_tpl  # Шаблон к которому нужно привести
+
+    def tpl_to_regex(self, tpl: str) -> str:
+        """Заменить шаблоны вида {маска} на именованные регулярные выражения"""
+        return re.sub(self.MASK_REGEXP, "(?P<\g<tpl_name>>.+)", tpl)
+
+
+    def tpl_to_sub(self, tpl: str) -> str:
+        """Заменить шаблоны вида {ШАБЛОН} на подстановочные выражения"""
+        # HACK: не получилось сразу заменить backslash. Возможно это
+        # изменится в будущих версиях питона.
+        s = re.sub(self.MASK_REGEXP, rf"{self.NOT_USED_SYM}g<\g<tpl_name>>", tpl)
+        return s.replace(self.NOT_USED_SYM, "\\")
+
+
+    def transform(self, topic: str) -> str | None:
+        """Получить топик путём подстановки в шаблон"""
+        regex_str = self.tpl_to_regex(self.src_tpl)
+        is_match = re.match(regex_str, topic)
+        if not is_match:
+            return None
+
+        sub_mask = self.tpl_to_sub(self.dst_tpl)
+        return re.sub(regex_str, sub_mask, topic)
---
+++ tests/test_utils.py
@@ -0,0 +1,21 @@
+from connector.utils import Template
+
+def test_template_topic():
+    t = Template(
+        'customer/{customer_id}/dev/{device_id}/v{schema_version}',
+        'customer_{customer_id}',
+    )
+    res = t.transform('not_match')
+    assert res is None
+
+    res = t.transform('customer/my_customer_id/dev/my_device_id/v1')
+    assert res == 'customer_my_customer_id'
+
+def test_template_header():
+    t = Template(
+        'customer/{customer_id}/dev/{device_id}/v{schema_version}',
+        'schema_version:{schema_version}|device_id:{device_id}',
+    )
+
+    res = t.transform('customer/my_customer_id/dev/my_device_id/v1')
+    # assert res == 'schema_version:1|device_id:my_device_id'
