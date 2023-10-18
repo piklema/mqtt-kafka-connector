a1e07eedc1165c293550e4acd74c778aa9e197d2
srgi0 <sergey.dubovitsky@piklema.com>
2023-10-18 08:01:57 +0300

    fix context vars

--- src/mqtt_kafka_connector/context_vars.py
+++ src/mqtt_kafka_connector/context_vars.py
@@ -6,11 +6,13 @@ from sentry_sdk import set_tag
 MESSAGE_UUID = 'message_uuid'
 DEVICE_ID = 'device_id'
 
-message_uuid_var = ContextVar(MESSAGE_UUID, default=uuid.uuid4().hex)
+message_uuid_var = ContextVar(MESSAGE_UUID, default='')
 device_id_var = ContextVar(DEVICE_ID, default=0)
 
 
-def setup_vars(device_id: int):
+def setup_context_vars(device_id: int):
+    uuid_hex = uuid.uuid4().hex
+    message_uuid_var.set(uuid_hex)
     device_id_var.set(device_id)
     set_tag(DEVICE_ID, device_id)
-    set_tag(MESSAGE_UUID, message_uuid_var.get())
+    set_tag(MESSAGE_UUID, uuid_hex)
