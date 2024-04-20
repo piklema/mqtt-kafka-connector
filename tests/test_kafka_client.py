263dc04608750af1e40fea4a618d4504f21e3f94
srgi0 <dss181@gmail.com>
2024-04-20 11:47:03 +0300

    NGUK-1603 kafka fixture

--- tests/test_kafka_client.py
+++ tests/test_kafka_client.py
@@ -1,4 +1,9 @@
-from unittest.mock import AsyncMock, patch, MagicMock
+import asyncio
+from unittest import mock
+from unittest.mock import AsyncMock, MagicMock, patch
+
+import pytest
+
 from mqtt_kafka_connector.clients.kafka import KafkaProducer
 from mqtt_kafka_connector.conf import (
     KAFKA_BOOTSTRAP_SERVERS,
@@ -7,66 +12,63 @@ from mqtt_kafka_connector.conf import (
 )
 
 
-@patch('mqtt_kafka_connector.clients.kafka.AIOKafkaProducer')
-def test_kafka_producer_init(mock_producer):
-    KafkaProducer('loop')
-    mock_producer.assert_called_once_with(
-        bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS, loop='loop'
-    )
+@pytest.fixture(scope="module")
+def loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 
-@patch('mqtt_kafka_connector.clients.kafka.AIOKafkaProducer')
-async def test_start(mock_producer):
-    mock_producer_instance = mock_producer.return_value
-    mock_producer_instance.start = AsyncMock()
-
-    kafka_producer = KafkaProducer('loop')
-    await kafka_producer.start()
-
-    mock_producer_instance.start.assert_called_once()
-
-
-@patch('mqtt_kafka_connector.clients.kafka.AIOKafkaProducer')
-async def test_stop(mock_producer):
-    mock_producer_instance = mock_producer.return_value
-    mock_producer_instance.stop = AsyncMock()
-
-    kafka_producer = KafkaProducer('loop')
-    await kafka_producer.stop()
-
-    mock_producer_instance.stop.assert_called_once()
-
-
-@patch('mqtt_kafka_connector.clients.kafka.AIOKafkaProducer')
-async def test_send_batch(mock_producer):
-    mock_producer_instance = mock_producer.return_value
-
-    mock_producer_instance.create_batch = MagicMock()
-    mock_producer_instance.create_batch.return_value.append.side_effect = [None, '1', 'metadata' ]
-    mock_producer_instance.partitions_for = AsyncMock(return_value=[0])
-    mock_producer_instance.send_batch = AsyncMock()
-
-    kafka_producer = KafkaProducer('loop')
-    await kafka_producer.send_batch('topic', 'headers', ['message1', 'message2'])
-
-    mock_producer_instance.create_batch.assert_called()
-    mock_producer_instance.partitions_for.assert_called_with('topic')
-    mock_producer_instance.send_batch.assert_called()
-
-
-@patch('mqtt_kafka_connector.clients.kafka.AIOKafkaProducer')
-async def test_send(mock_producer):
-    mock_producer_instance = mock_producer.return_value
+@pytest.fixture()
+def kafka_producer(loop):
+    mock_producer = AsyncMock()
+    mock_producer.start = AsyncMock()
+    mock_producer.stop = AsyncMock()
+    mock_producer.create_batch = MagicMock()
+    mock_producer.create_batch.return_value.append.side_effect = [
+        None,
+        '1',
+        'metadata',
+    ]
+    mock_producer.partitions_for = AsyncMock(return_value=[0])
+    mock_producer.send_batch = AsyncMock()
 
     async def send_result():
         res = MagicMock()
-        res.partition = 10
+        res.partition = 0
         return res
 
-    mock_producer_instance.send = AsyncMock(return_value=True, side_effect=[send_result()])
+    mock_producer.send = AsyncMock(
+        return_value=True, side_effect=[send_result()]
+    )
 
-    kafka_producer = KafkaProducer('loop')
-    result = await kafka_producer.send('topic', {'message': 'test'}, b'key', [('header', b'value')])
+    kafka_producer = KafkaProducer(loop)
+    kafka_producer.producer = mock_producer
+    return kafka_producer
 
-    mock_producer_instance.send.assert_called_with('topic', value=kafka_producer._prepare_msg_for_kafka({'message': 'test'}), key=b'key', headers=[('header', b'value')])
+
+async def test_send_batch(kafka_producer):
+    await kafka_producer.send_batch(
+        'topic', 'headers', ['message1', 'message2']
+    )
+
+    kafka_producer.producer.create_batch.assert_called()
+    kafka_producer.producer.partitions_for.assert_called_with('topic')
+    kafka_producer.producer.send_batch.assert_called()
+
+
+async def test_send(kafka_producer):
+    result = await kafka_producer.send(
+        'topic', {'message': 'test'}, b'key', [('header', b'value')]
+    )
+
+    kafka_producer.producer.send.assert_called_with(
+        'topic',
+        value=kafka_producer._prepare_msg_for_kafka({'message': 'test'}),
+        key=b'key',
+        headers=[('header', b'value')],
+    )
     assert result is True
